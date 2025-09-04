8338b8d727182100baaed6638d393c8b8e18564f
Youwen Wu <youwenw@gmail.com>
2025-09-04 05:17:03 -0700

    fixup! feat: switch display manager from tuigreet to ly

--- modules/linux/greeter/default.nix
+++ modules/linux/greeter/default.nix
@@ -36,7 +36,7 @@ in
         vi_mode = false;
         colormix_col1 = "0x00${config.lib.stylix.colors.base00}";
         colormix_col2 = "0x00${config.lib.stylix.colors.base0F}";
-        colormix_col3 = "0x00${config.lib.stylix.colors.base08}";
+        colormix_col3 = "0x00${config.lib.stylix.colors.base07}";
       };
     };
   };
