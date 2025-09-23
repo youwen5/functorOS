603941e7600cd409535b1abe153210f4c294918e
Youwen Wu <youwenw@gmail.com>
2025-09-22 20:34:35 -0700

    fix: guard spicetify behind x86_64-linux
    closes #10

--- hm/modules/linux/spicetify/default.nix
+++ hm/modules/linux/spicetify/default.nix
@@ -3,26 +3,36 @@
   config,
   osConfig,
   lib,
+  pkgs,
   ...
 }:
 {
-  config = lib.mkIf (config.functorOS.programs.enable && osConfig.functorOS.config.allowUnfree) {
-    programs.spicetify = {
-      enable = true;
-      # theme = spicepkgs.themes.dribbblish;
-      # colorScheme = "rosepine";
-      enabledExtensions = with spicepkgs.extensions; [
-        lastfm
-        fullAppDisplayMod
-        adblock
-        shuffle
-        fullAlbumDate
-        featureShuffle
-        showQueueDuration
-      ];
-      enabledCustomApps = with spicepkgs.apps; [
-        lyricsPlus
-      ];
-    };
-  };
+  config =
+    lib.mkIf
+      (
+        config.functorOS.programs.enable
+        && osConfig.functorOS.config.allowUnfree
+        && pkgs.stdenv.hostPlatform.isLinux
+        && pkgs.stdenv.hostPlatform.isx86_64
+      )
+      {
+        programs.spicetify = {
+          enable = true;
+          # theme = spicepkgs.themes.dribbblish;
+          # colorScheme = "rosepine";
+          enabledExtensions = with spicepkgs.extensions; [
+            lastfm
+            fullAppDisplayMod
+            adblock
+            shuffle
+            fullAlbumDate
+            featureShuffle
+            showQueueDuration
+          ];
+          enabledCustomApps = with spicepkgs.apps; [
+            lyricsPlus
+          ];
+        };
+      };
 }
+
