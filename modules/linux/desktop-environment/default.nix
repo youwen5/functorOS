4ae616bffb9575eff3770617481e5a1c0ddf8f50
Youwen Wu <youwenw@gmail.com>
2026-02-26 10:44:01 +0800

    feat: create functoros-desktop

--- modules/linux/desktop-environment/default.nix
+++ modules/linux/desktop-environment/default.nix
@@ -37,19 +37,61 @@ in
       withUWSM = true;
     };
 
-    nixpkgs.overlays = [
-      (final: prev: {
-        hyprland = prev.hyprland.overrideAttrs (
-          finalAttrs: prevAttrs: {
-            nativeBuildInputs = prevAttrs.nativeBuildInputs ++ [ prev.makeWrapper ];
-            postInstall = prevAttrs.postInstall + ''
-              wrapProgram $out/bin/start-hyprland \
-                --add-flags "--no-nixgl"
-            '';
-          }
-        );
-      })
+    environment.systemPackages = [
+      (
+        let
+          desktop-file = pkgs.writeText "hyprland-functoros.desktop" ''
+            [Desktop Entry]
+            Name=Hyprland (functorOS)
+            Comment=An intelligent dynamic tiling Wayland compositor
+            Exec=${config.programs.hyprland.package}/bin/start-hyprland --no-nixgl
+            Type=Application
+            DesktopNames=Hyprland
+            Keywords=tiling;wayland;compositor;
+          '';
+          uwsm-file = pkgs.writeText "hyprland-uwsm-functoros.desktop" ''
+            [Desktop Entry]
+            Name=functorOS Desktop
+            Comment=An intelligent dynamic tiling Wayland compositor
+            Exec=uwsm start -e -D Hyprland hyprland-functoros.desktop
+            TryExec=uwsm
+            DesktopNames=Hyprland
+            Type=Application
+          '';
+        in
+        pkgs.stdenvNoCC.mkDerivation {
+          pname = "functoros-hyprland-desktop";
+          version = config.programs.hyprland.package.version;
+
+          phases = [ "installPhase" ];
+
+          installPhase = ''
+            mkdir -p $out/share/wayland-sessions
+            cp ${desktop-file} $out/share/wayland-sessions/hyprland-functoros.desktop
+            cp ${uwsm-file} $out/share/wayland-sessions/hyprland-uwsm-functoros.desktop
+          '';
+
+          passthru.providedSessions = [
+            "hyprland-functoros"
+            "hyprland-uwsm-functoros.desktop"
+          ];
+        }
+      )
     ];
+
+    # nixpkgs.overlays = [
+    #   (final: prev: {
+    #     hyprland = prev.hyprland.overrideAttrs (
+    #       finalAttrs: prevAttrs: {
+    #         nativeBuildInputs = prevAttrs.nativeBuildInputs ++ [ prev.makeWrapper ];
+    #         postInstall = prevAttrs.postInstall + ''
+    #           wrapProgram $out/bin/start-hyprland \
+    #             --add-flags "--no-nixgl"
+    #         '';
+    #       }
+    #     );
+    #   })
+    # ];
 
     programs.uwsm.enable = true;
 
