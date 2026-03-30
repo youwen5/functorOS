fcd809c7cc3da9b6af3bfd7eae46a783024068d4
Ananth <ananth@functor.systems>
2026-03-30 13:23:32 -0700

    Revert "fix: use forge-hosted identity repo"
    This reverts commit cab3920943d5e95a473c0699c82b9e910342a3d6.

--- pkgs/by-name/functor-systems-icons/default.nix
+++ pkgs/by-name/functor-systems-icons/default.nix
@@ -1,8 +1,9 @@
 {
-  fetchgit,
+  fetchFromGitHub,
 }:
-fetchgit {
-  url = "https://code.functor.systems/functor.systems/identity.git";
+fetchFromGitHub {
+  owner = "youwen5";
+  repo = "functor.systems-identity";
   rev = "9a04adb3b5b0d8dc089550f307aee465451aff4b";
   sparseCheckout = [
     "icons"
