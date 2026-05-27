232bd1fd9c862918b57b4ee6aaf19a98a8a8b450
Ananth <ananth@functor.systems>
2026-05-26 19:06:46 -0700

    feat: ollama

--- modules/linux/graphics/default.nix
+++ modules/linux/graphics/default.nix
@@ -61,6 +61,11 @@ in
     };
 
     services.xserver.videoDrivers = lib.mkIf cfg.nvidia.enable [ "nvidia" ];
+    services.ollama = {
+      enable = true;
+      package = lib.mkIf cfg.nvidia.enable pkgs.ollama-cuda;
+      loadModels = [ "phi4-reasoning" "phi4-mini-reasoning" "qwen2.5-coder:7b" ];
+    };
 
     functorOS.config.extraUnfreePackages = lib.mkIf cfg.nvidia.enable [
       "nvidia-x11"
