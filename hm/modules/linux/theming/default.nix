61d0716cc4c892aeeac7fd4182dcfaeaa611d1c4
Youwen Wu <youwenw@gmail.com>
2026-07-21 02:46:52 +0900

    chore: format on theming

--- hm/modules/linux/theming/default.nix
+++ hm/modules/linux/theming/default.nix
@@ -7,7 +7,6 @@
   imports = [ ./stylix.nix ];
   gtk = {
     enable = true;
-    gtk4.theme = null;
     iconTheme = {
       name = if (osConfig.stylix.polarity == "dark") then "Papirus-Dark" else "Papirus-Light";
     };
