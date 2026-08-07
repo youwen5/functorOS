b2d71b1afcde23167ce088865f0b0c72bdb8f369
Ananth <ananth@functor.systems>
2026-08-07 11:00:25 +0530

    fix: nuclear

--- hm/modules/linux/theming/stylix.nix
+++ hm/modules/linux/theming/stylix.nix
@@ -1,15 +1,4 @@
 { osConfig, lib, ... }:
 {
-  config = lib.mkIf osConfig.functorOS.theming.enable {
-    stylix.targets = {
-      waybar.enable = false;
-      kitty.variant256Colors = true;
-      neovim.enable = false;
-      kde.enable = true;
-      gnome.enable = true;
-      starship.enable = false;
-      rofi.enable = false;
-      mako.enable = false;
-    };
-  };
+  config = lib.mkIf osConfig.functorOS.theming.enable {};
 }
